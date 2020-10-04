1a9317936112e2caae6dad75bffd211f36682826
Lienhard Laurent <laurent.lienhard@polehabitatstrasbourg.fr>
2020-10-04 15:07:55 +0200

    commit

--- 03_AvecConfigurationDataFile/ConfigurationData.psd1
+++ 03_AvecConfigurationDataFile/ConfigurationData.psd1
@@ -1,41 +1,45 @@
 ﻿@{
-    AllNodes  = @(
-            @{
-                NodeName = "*"
-                WindowsFeature = "SNMP-Service", "SNMP-WMI-Provider"
-                Services       = "SNMP"
-                Ensure  = "Present"
-                ServiceState = "Running"
-                ServiceStartupType = "Automatic"
-            }
-            @{
-                NodeName = "SRV01"
-                Role     = @("PullServer","DomainController","RootCA","Routeur")
-            }
-            @{
-                NodeName = @("SERVER1","SERVER2")
-                Role = "RdsServer"
-            }
-        )
-    Roles = @(
-            @{
-                RoleName = "PullServer"
-                Modules = "PSDesiredStateConfiguration", "chocolatey", "ComputerManagementDsc", "SqlServerDsc", "ActiveDirectoryDsc"
-            }
-            @{
-                RoleName = "DomainController"
-                WindowsFeature = "AD-Domain-Services","RSAT-AD-PowerShell"
-                Services = "DNS", "NTDS"
-            }
-            @{
-                RoleName = "RdsServer"
-                WindowsFeature = "RDS-RD-Server"
-                ChocoPackages  = "javaruntime" , "7zip.install"
-            }
-        )
+    AllNodes    = @(
+        @{
+            NodeName           = "*"
+            WindowsFeature     = @("SNMP-Service", "SNMP-WMI-Provider")
+            Services           = "SNMP"
+            Ensure             = "Present"
+            ServiceState       = "Running"
+            ServiceStartupType = "Automatic"
+        },
+        @{
+            NodeName = "SRV01"
+            Role     = @("PullServer", "DomainController", "RootCA", "Routeur")
+        },
+        @{
+            NodeName = "SERVER01"
+            Role     = @("RdsServer")
+        }
+        @{
+            NodeName = "SERVER02"
+            Role     = @("RdsServer")
+        }
+    )
+    Roles       = @(
+        @{
+            RoleName = "PullServer"
+            Modules  = @("PSDesiredStateConfiguration", "chocolatey", "ComputerManagementDsc", "SqlServerDsc", "ActiveDirectoryDsc")
+        },
+        @{
+            RoleName       = "DomainController"
+            WindowsFeature = @("AD-Domain-Services", "RSAT-AD-PowerShell")
+            Services       = @("DNS", "NTDS")
+        },
+        @{
+            RoleName       = "RdsServer"
+            WindowsFeature = @("RDS-RD-Server")
+            ChocoPackages  = @("javaruntime" , "7zip.install")
+        }
+    )
     ChocoParams = @{
         ChocoInstallDir = "$env:ProgramData\Chocolatey"
-        ChocoTempDir    = "$env:Temp\Chocolatey"
+        ChocoTempDir    = "C:\temp\Chocolatey"
         ChocoSource     = 'https://chocolatey.org/api/v2'
     }
     LCMParams = @{
--- 03_AvecConfigurationDataFile/ConfigurationServer.ps1
+++ 03_AvecConfigurationDataFile/ConfigurationServer.ps1
@@ -1,172 +1,143 @@
-﻿Configuration ConfigurationServer
-{
-    Import-DscResource –ModuleName PSDesiredStateConfiguration, chocolatey, ComputerManagementDsc, SqlServerDsc, PowershellModule
+﻿Configuration ConfigurationServer {
+    Import-DscResource –ModuleName PSDesiredStateConfiguration, chocolatey, ComputerManagementDsc
+    #Import-DscResource -ModuleName PowerShellGet -ModuleVersion "2.2.3"
 
     #Configure All Node
-    Node $AllNodes.NodeName
-    {
+    Node $AllNodes.NodeName {
         #Install feature for All Node
-        Foreach ($Feature in $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.WindowsFeature) {
+        foreach ($Feature in $Node.WindowsFeature) {
             WindowsFeature $Feature {
                 Name   = "$Feature"
-                Ensure = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
+                Ensure = $Node.Ensure
             }
         }
-
         #Install services for All Node
-        foreach ($Service in $ConfigurationData.AllNodes.where{$_.NodeName -Contains $NodeName}.Services) {
+        foreach ($Service in $Node.Services) {
             Service $Service {
                 Name        = "$Service"
-                Ensure      = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-                StartupType = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceStartupType
-                State       = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceState
+                Ensure      = $Node.Ensure
+                StartupType = $Node.ServiceStartupType
+                State       = $Node.ServiceState
             }
         }
-    }
-
-    #Configure All Pull Server
-    Node $AllNodes.Where{ $_.Role -Contains "PullServer" }.NodeName
-    {
-        #Install feature for Pull Server
-        Foreach ($Feature in $ConfigurationData.Roles.Where{ $Node.Role -Contains $_.RoleName }.WindowsFeature) {
-            WindowsFeature $Feature {
-                Name   = "$Feature"
-                Ensure = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
+        #Configure All Pull Server
+        if ($Node.Role -contains "PullServer") {
+            #Install feature for Pull Server
+            foreach ($Feature in $ConfigurationData.Roles.Where{ $_.RoleName -Contains "PullServer" }.WindowsFeature) {
+                WindowsFeature $Feature {
+                    Name   = "$Feature"
+                    Ensure = $Node.Ensure
+                }
             }
-        }
-
-        #Install module for Pull Server
-        Foreach ($Module in $ConfigurationData.Roles.Where{ $Node.Role -Contains $_.RoleName }.Modules) {
-            PSModuleResource "$Module" {
-                Module_Name = "$Module"
-                Ensure       = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-                InstallScope = "AllUsers"
+            #Install module for Pull Server
+<#             foreach ($Module in $ConfigurationData.Roles.Where{ $_.RoleName -Contains "PullServer" }.Modules) {
+                PSModule "$Module" {
+                    Name   = "$Module"
+                    Ensure = $Node.Ensure
+                }
+            } #>
+            #Install Service for Pull Server
+            foreach ($Service in $ConfigurationData.Roles.Where{ $_.RoleName -Contains "PullServer" }.Services) {
+                Service $Service {
+                    Name        = "$Service"
+                    Ensure      = $Node.Ensure
+                    StartupType = $Node.ServiceStartupType
+                    State       = $Node.ServiceState
+                }
             }
-        }
-
-        #Install Service for Pull Server
-        Foreach ($Service in $ConfigurationData.Roles.Where{ $Node.Role -Contains $_.RoleName }.Services) {
-            Service $Service {
-                Name        = "$Service"
-                Ensure      = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-                StartupType = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceStartupType
-                State       = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceState
+            #region <Config Directory>
+            File "RepTest" {
+                DestinationPath = "C:\Test"
+                Ensure          = "Present"
+                Type            = "Directory"
             }
+            File "RepConfiguration" {
+                DestinationPath = "C:\Test\Configuration"
+                Ensure          = "Present"
+                Type            = "Directory"
+                DependsOn       = "[File]RepTest"
+            }
+            File "RepMOFFiles" {
+                DestinationPath = "C:\Test\MOFFiles"
+                Ensure          = "Present"
+                Type            = "Directory"
+                DependsOn       = "[File]RepTest"
+            }
+            File "RepLCMFiles" {
+                DestinationPath = "C:\Test\LCMFiles"
+                Ensure          = "Present"
+                Type            = "Directory"
+                DependsOn       = "[File]RepTest"
+            }
+            File "RepSources" {
+                DestinationPath = "C:\Sources"
+                Type            = "Directory"
+                Ensure          = "Present"
+            }
+            SmbShare 'RepSources' {
+                Ensure      = "Present"
+                Path        = "C:\Sources"
+                Name        = "Sources"
+                Description = "Sources d'installation"
+                FullAccess  = @('Everyone')
+                DependsOn   = "[File]RepSources"
+            }
+            #endregion <Config Directory>
         }
-        #region <Config Directory>
-        File "RepTest" {
-            DestinationPath = "C:\Test"
-            Ensure          = "Present"
-            Type            = "Directory"
-        }
 
-        File "RepConfiguration" {
-            DestinationPath = "C:\Test\Configuration"
-            Ensure          = "Present"
-            Type            = "Directory"
-            DependsOn       = "[File]RepTest"
-        }
-
-        File "RepMOFFiles" {
-            DestinationPath = "C:\Test\MOFFiles"
-            Ensure          = "Present"
-            Type            = "Directory"
-            DependsOn       = "[File]RepTest"
-        }
-
-        File "RepLCMFiles" {
-            DestinationPath = "C:\Test\LCMFiles"
-            Ensure          = "Present"
-            Type            = "Directory"
-            DependsOn       = "[File]RepTest"
-        }
-
-        File "RepSources" {
-            DestinationPath = "C:\Sources"
-            Type            = "Directory"
-            Ensure          = "Present"
-        }
-
-        SmbShare 'RepSources' {
-            Ensure      = "Present"
-            Path        = "C:\Sources"
-            Name        = "Sources"
-            Description = "Sources d'installation"
-            FullAccess  = @('Everyone')
-            DependsOn   = "[File]RepSources"
-        }
-        #endregion <Config Directory>
-
-    }
-
-
-    #Configure All Domain Controller
-    Node $AllNodes.Where{ $_.Role -Contains "DomainController" }.NodeName
-    {
-        #Install feature for Domain Controller
-        Foreach ($Feature in $ConfigurationData.Roles.Where{$_.RoleName -Contains $Node.Role}.WindowsFeature)
-        {
-            WindowsFeature $Feature {
-                Name   = "$Feature"
-                Ensure      = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
+        #Configure All Domain Controller
+        if ($Node.Role -contains 'DomainController') {
+            foreach ($Feature in $ConfigurationData.Roles.Where{ $_.RoleName -Contains 'DomainController' }.WindowsFeature) {
+                WindowsFeature "DC_$Feature" {
+                    Name   = "$Feature"
+                    Ensure = $Node.Ensure
+                }
+            }
+            #Install services for Domain Controller
+            foreach ($Service in $ConfigurationData.Roles.where{ $_.RoleName -Contains 'DomainController' }.Services) {
+                Service $Service {
+                    Name        = "$Service"
+                    Ensure      = $Node.Ensure
+                    StartupType = $Node.ServiceStartupType
+                    State       = $Node.ServiceState
+                }
             }
         }
 
-        #Install services for Domain Controller
-        foreach ($Service in $ConfigurationData.Roles.where{ $_.RoleName -Contains $Node.Role }.Services)
-        {
-            Service $Service
-            {
-                Name = "$Service"
-                Ensure      = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-                StartupType = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceStartupType
-                State       = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.ServiceState
+        if ($Node.Role -Contains 'RdsServer' ) {
+            #Install feature for Rds Server
+            foreach ($Feature in $ConfigurationData.Roles.Where{ $_.RoleName -Contains 'RdsServer' }.WindowsFeature) {
+                WindowsFeature "RDS_$Feature" {
+                    Name   = $Feature
+                    Ensure = $Node.Ensure
+                }
             }
-        }
-    }
-
-    #Configure All Rds Server
-    Node $AllNodes.Where{ $_.Role -Contains "RdsServer" }.NodeName
-    {
-        #Install feature for Rds Server
-        Foreach ($Feature in $ConfigurationData.Roles.Where{ $_.RoleName -Contains $Node.Role }.WindowsFeature) {
-            WindowsFeature $Feature {
-                Name   = "$Feature"
-                Ensure = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
+            #region <ChocoPackage>
+            #Install Chocolatey on Server RDS
+            ChocolateySoftware ChocoInst {
+                Ensure                = $Node.Ensure
+                InstallationDirectory = $ConfigurationData.ChocoParams.ChocoInstallDir
+                ChocoTempDir          = $ConfigurationData.ChocoParams.ChocoSource
             }
+            #Configure Chocolatey on Server RDS
+            ChocolateySource ChocolateyOrg {
+                DependsOn = '[ChocolateySoftware]ChocoInst'
+                Ensure    = $Node.Ensure
+                Name      = 'Chocolatey'
+                Source    = $ConfigurationData.ChocoParams.ChocoTempDir
+                Priority  = 0
+                Disabled  = $false
+            }
+            #endregion <ChocoPackage>
         }
-
-        #region <ChocoPackage>
-        #Install Chocolatey on Server RDS
-        ChocolateySoftware ChocoInst {
-            Ensure                = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-            InstallationDirectory = $ConfigurationData.ChocoParams.ChocoInstallDir
-            ChocoTempDir          = $ConfigurationData.ChocoParams.ChocoSource
-        }
-
-        #Configure Chocolatey on Server RDS
-        ChocolateySource ChocolateyOrg {
-            DependsOn = '[ChocolateySoftware]ChocoInst'
-            Ensure    = $ConfigurationData.AllNodes.Where{ $_.NodeName -Contains $NodeName }.Ensure
-            Name      = 'Chocolatey'
-            Source    = $ConfigurationData.ChocoParams.ChocoTempDir
-            Priority  = 0
-            Disabled  = $false
-        }
-        #endregion <ChocoPackage>
-
     }
 }
-
 ConfigurationServer -ConfigurationData ".\ConfigurationData.psd1" -OutputPath ".\MOFFiles"
-
-<# Get-ChildItem -Path ".\MOFFiles" | Foreach {
+<# Get-ChildItem -Path C:\Test\MOF | foreach {
 $MofName = $_.Name
 $DestinationMof = "C:\Program Files\WindowsPowershell\DSCService\Configuration\$MofName"
 Copy-Item $_.FullName $DestinationMof
 New-DscChecksum $DestinationMof -force
 } #>
-
 #Start-DscConfiguration -Wait -Verbose -Path "C:\Test\Dsc" -Force
-
 #Test-DscConfiguration -Path "C:\Test\Dsc"
